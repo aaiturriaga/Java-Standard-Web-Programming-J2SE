2c7ae6dd720860823e94936ba3d295d6fbb5ad36
iturriaga <iturriaga@BAIN0479.mshome.net>
2020-06-28 13:25:51 -0300

    Clase 11 -login -uso de header y barra de navegacion

---
+++ domain/src/main/java/ar/com/educacionit/domain/dto/LoginDTO.java
@@ -0,0 +1,40 @@
+package ar.com.educacionit.domain.dto;
+
+public class LoginDTO {
+
+	private String userName;
+	
+	private String password;
+
+	public LoginDTO(String userName, String password) {
+		super();
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public LoginDTO() {
+		super();
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginModel [userName=" + userName + ", password=" + password + "]";
+	}
+	
+}
---
+++ webapp/src/main/java/ar/com/educacionit/web/servlet/ListadoProductosServlet.java
@@ -0,0 +1,35 @@
+package ar.com.educacionit.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ar.com.educacionit.services.ProductoServices;
+import ar.com.educacionit.services.ProductoServicesImpl;
+
+/**
+ * READ -> GET
+ * @author CX PC
+ *
+ */
+@WebServlet(urlPatterns = "/listado")
+public class ListadoProductosServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// guardar informacion en el request
+		// para mostrarlo en una jsp
+		ProductoServices ps = new ProductoServicesImpl();
+		try {
+			req.getSession().setAttribute("productos", ps.findProductos());
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		//redireccion req + resp
+		req.getRequestDispatcher("listado.jsp").forward(req, resp);
+	}
+}
